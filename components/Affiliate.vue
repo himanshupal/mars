bcae6d8cd50164a459281f10a2c8f5bab1f1d818
gokusleeping <65029523+gokusleeping@users.noreply.github.com>
2021-08-14 12:44:35 +0530

    first affliate link

--- components/Affiliate.vue
+++ components/Affiliate.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="flex flex-col text-center gap-6 py-10 bg-gray-800 items-center">
-    <a
-      target="blank"
-      href="https://www.amazon.in/gp/product/9325292629/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=9325292629&linkCode=as2&tag=eminem04-21&linkId=1c6eed31e1c48c6d8aacd3d69c1c99e6"
-    >
-      <img
-        border="0"
-        src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=9325292629&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21&tag=eminem04-21"
-      />
-    </a>
+<a target="_blank"  href="https://www.amazon.in/gp/product/B099T7XNGW/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B099T7XNGW&linkCode=as2&tag=eminem04-21&linkId=2ab716b704c10de63c5f610ea22971a2"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=B099T7XNGW&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21" ></a>
   </div>
 </template>
 
