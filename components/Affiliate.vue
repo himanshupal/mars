35fb1201e0c8e0595eb9a1f63f25eee92db64ac7
gokusleeping <65029523+gokusleeping@users.noreply.github.com>
2021-08-14 12:56:38 +0530

    adding three links at a time

--- components/Affiliate.vue
+++ components/Affiliate.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-wrap justify-center text-center gap-6 py-10 bg-gray-800 items-center">
-<a target="_blank"  href="https://www.amazon.in/gp/product/B099T7XNGW/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B099T7XNGW&linkCode=as2&tag=eminem04-21&linkId=2ab716b704c10de63c5f610ea22971a2"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=B099T7XNGW&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21" ></a>
-  </div>
+<a target="_blank"  href="https://www.amazon.in/gp/product/B099T7XNGW/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B099T7XNGW&linkCode=as2&tag=eminem04-21&linkId=2ab716b704c10de63c5f610ea22971a2"><img rounded border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=B099T7XNGW&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21" ></a>
+ <a target="_blank"  href="https://www.amazon.in/gp/product/B099T7XNGW/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B099T7XNGW&linkCode=as2&tag=eminem04-21&linkId=2ab716b704c10de63c5f610ea22971a2"><img rounded border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=B099T7XNGW&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21" ></a>
+ <a target="_blank"  href="https://www.amazon.in/gp/product/B079Q17WXZ/ref=as_li_tl?ie=UTF8&camp=3638&creative=24630&creativeASIN=B079Q17WXZ&linkCode=as2&tag=eminem04-21&linkId=0a3209f123bdd4cad790725a6ba5f1ed"><img border="0" src="//ws-in.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=IN&ASIN=B079Q17WXZ&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=eminem04-21" ></a>
+ </div>
 </template>
 
 <script lang="ts">
